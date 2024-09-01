3d24a656122ac182f295600609871b68331976b9
Amit Koshti <amitkoshti@gmail.com>
2024-09-01 19:10:51 +0200

    modular programming

--- mainpg.py
+++ mainpg.py
@@ -1,35 +1,30 @@
 
 from modinput import fun_input
-from modadd import fun_add
-
-def fun_sub(num1, num2):
-    pass
-
-def fun_mul(num1, num2):
-    pass
-
-def fun_div(num1, num2):
-    pass
-
-def fun_output(result):
-    print("Result is ", result)
+from modadd   import fun_add
+from modsub   import fun_sub
+from modmult  import fun_mul
+from moddiv   import fun_div
+from modout   import fun_output
 
 def main():
     num1, num2, oper = fun_input()
+    
     print(num1, num2, oper)
+    
     result = 0
     match oper:
         case '+':
             result = fun_add(number1=num1, number2=num2, operator=oper)
+            
         case '-':
-            result = fun_sub(num1, num2)  
+            result = fun_sub(number1=num1, number2=num2, operator=oper) 
         
         case '*':
-            result = fun_mul(num1, num2)
+            result = fun_mul(number1=num1, number2=num2, operator=oper)
+            
         case '/':
-            result = fun_div(num1, num2) 
-    
-    print(result)        
+            result = fun_div(number1=num1, number2=num2, operator=oper)
+          
     fun_output(result)
     
 if __name__ == '__main__' :
