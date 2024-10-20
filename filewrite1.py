e5ebf5efad493d1c826c068b7366dbdf355c25a5
Amit Koshti <amitkoshti@gmail.com>
2024-10-20 22:27:36 +0200

    file hadling

--- filewrite1.py
+++ filewrite1.py
@@ -1,18 +1,18 @@
 
       
 try:
-    with  open("records.txt", "w+") as file_handle:
+    file_handle = open("records.txt", "a+")
         
        
-        records = "Sakshi;Gurme;sakshi.gurme@gmail.com;9312305544\n"
-        file_handle.write(records)
+    records = "Sakshi;Gurme;sakshi.gurme@gmail.com;9312305544\n"
+    file_handle.write(records)
         
-        file_handle.seek(89)
-        print(file_handle.tell())
-        records = file_handle.read()
-        print(records)
+    file_handle.seek(89)
+    print(file_handle.tell())
+    records = file_handle.read()
+    print(records)
         
-        """for _ in range(0,30):
+    """for _ in range(0,30):
            line = file_handle.readline()
            print(line)"""
        
@@ -26,6 +26,6 @@ except Exception as err:
     print(f"Error occuered while opening a file in write mode {err}")
 
 finally:
-  
+    
     print("Program ended")
     
